3ff06a1319078540f7a4f8807e553adfb75df909
Manas Mishra <soulstealer806@gmail.com>
2021-07-28 19:33:43 +0530

    basic javascript;

--- site/Employee form/first.js
+++ site/Employee form/first.js
@@ -20,19 +20,4 @@
     }
     document.write(greeting) ;
     var username;
-    (function() {
-        
-         var form= document.getElementByld('login ' );     
-        
-        addEvent(form,'submit',function(e)
-        {
-        e.preventDefault();
-        var elements= this.elements;
-        
-       
-        var username = elements.username.value;
-        
-        var msg = 'Welcome ' + username; 
-        document.getElementByld('main').textContent = msg;
-        } ) ;
-        } ());
+    
